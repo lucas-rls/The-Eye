1edfde19836116f1a3af4793333208790475cbb7
Lucas Ramalho Luiz da Silva <lucas.ramalho@b2wdigital.com>
2022-04-01 14:48:12 -0300

    feat: Create category and event models

--- collector/models.py
+++ collector/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=300, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                name="unique_session_timestamp", fields=["session_id", "timestamp"]
+            )
+        ]
+
+    session_id = models.CharField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    data = models.JSONField()
+    timestamp = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
